21905413cc8ae5b62c502f79f62361ceac726bdf
zmorok <dimaszhuk8358@gmail.com>
2024-03-17 12:55:17 +0300

    some changes

---
+++ src/sidebar.js
@@ -0,0 +1,7 @@
+const sidebar = document.querySelector('.sidebar-content')
+function showSidebar() {
+	sidebar.style.display = 'flex'
+}
+function closeSidebar() {
+	sidebar.style.display = 'none'
+}
